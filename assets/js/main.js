8b75c4de06257f8aa7905e30f991f15555ac8067
masudcse92 <masudcse9292@gmail.com>
2023-03-02 15:08:53 +0600

    toggle all done

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,3 +108,27 @@
 
 })(jQuery);
 
+
+// home page popup section toggle 
+function popupToggle(){
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbxJnCCeSltnALG6CNQVP-3MiMi74vjXoKf1gzRc_-iwIeujCkXOPnWvCnzFPoq2LHuS9w/exec';
+    const form = document.forms['submit-to-google-sheets'];
+    const successMessage = document.getElementById('success');
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+          success.innerHTML = "Data successfully message";
+          setTimeout(function(){
+              success.innerHTML = " ";
+          }, 5000)
+          form.reset();
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
+  
+  }
+  
+  popupToggle();
+  
